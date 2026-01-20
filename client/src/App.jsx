6a32a045b4c4f12774dd4c7a0bd19ef1b18e8ce1
umar <mohammad.akram.ug22@nsut.ac.in>
2026-01-20 19:47:12 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import { Route, Routes, useLocation } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./Pages/Home"
+import Movies from "./Pages/Movies"
+import MovieDetails from "./Pages/MovieDetails"
+import SeatLayout from "./Pages/SeatLayout"
+import MyBookings from "./Pages/MyBookings"
+import Favourite from "./Pages/Favourite"
+import {Toaster} from 'react-hot-toast'
+import Footer from "./components/Footer"
+ 
+const App = () => {
+
+  const isAdminRoute=useLocation().pathname.startsWith('/admin');
+ 
+  return (
+    <>
+
+    <Toaster/>
+
+    {/* main routing navbar and footer are default on each route */}
+    {!isAdminRoute && <Navbar/>} 
+   
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/movies" element={<Movies/>} />
+      <Route path="/movies/:id" element={<MovieDetails/>} />
+      <Route path="/movies/:id/:date" element={<SeatLayout/>} />
+      <Route path="/my-bookings" element={<MyBookings/>} />
+      <Route path="/favourite" element={<Favourite/>} />
+
+
+    
+
+    </Routes>
+
+
+      {!isAdminRoute && <Footer/>} 
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+ 
+export default App
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import FeatureSection from "../components/FeatureSection"
+import Hero from "../components/Hero"
+import TrailerSection from "../components/TrailerSection"
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureSection/>
+      <TrailerSection/>
+      
+    </>
+  )
+}
+
+export default Home
