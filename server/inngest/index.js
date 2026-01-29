6afe95196ac57e6a6857fdd7940b74ea9ee607c1
umar <mohammad.akram.ug22@nsut.ac.in>
2026-01-30 00:02:33 +0530

    hi

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -12,11 +12,14 @@ const syncUserCreation = inngest.createFunction(
   { event: "clerk/user.created" },
   async ({ event }) => {
 
+     await connectDB();
+     
+
 
     const {id,first_name,last_name,email_addresses,image_url}=event.data
 
     const userData={
-        _id:id,
+        
         email:email_addresses[0].email_address,
         name: first_name + ' ' + last_name,
         image:image_url
